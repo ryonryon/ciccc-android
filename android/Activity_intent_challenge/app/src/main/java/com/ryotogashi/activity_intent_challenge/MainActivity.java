f3cefdebc180a9a5e7722978971918da4f3afb9a
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-24 11:01:34 -0700

    add activity and intent challenge

---
+++ android/Activity_intent_challenge/app/src/main/java/com/ryotogashi/activity_intent_challenge/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ryotogashi.activity_intent_challenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOne(View view) {
+    }
+
+    public void buttonTwo(View view) {
+    }
+
+    public void buttonThree(View view) {
+    }
+}
