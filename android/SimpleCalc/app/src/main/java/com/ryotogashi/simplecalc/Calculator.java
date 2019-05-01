84d6febd536bfb75fdbea0dda655cc9f79300101
ryotogashi <ryo.togashi.ca@gmail.com>
2019-05-01 13:46:54 -0700

    simple calc

--- android/SimpleCalc/app/src/main/java/com/ryotogashi/simplecalc/Calculator.java
+++ android/SimpleCalc/app/src/main/java/com/ryotogashi/simplecalc/Calculator.java
@@ -1,4 +1,35 @@
 package com.ryotogashi.simplecalc;
 
+/**
+ * Utility class for SimpleCalc to perform the actual calculations.
+ */
 public class Calculator {
+
+    /**
+     * Addition operation
+     */
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
 }
--- android/SimpleCalc/app/src/test/java/com/ryotogashi/simplecalc/CalculatorTest.java
+++ android/SimpleCalc/app/src/test/java/com/ryotogashi/simplecalc/CalculatorTest.java
@@ -1,17 +1,39 @@
 package com.ryotogashi.simplecalc;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+@RunWith(JUnit4.class)
+public class CalculatorTest {
+
+    private Calculator mCalculator;
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mCalculator = new Calculator();
     }
+
+    /**
+     * Test for simple addition
+     */
+    @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
+
+
 }
