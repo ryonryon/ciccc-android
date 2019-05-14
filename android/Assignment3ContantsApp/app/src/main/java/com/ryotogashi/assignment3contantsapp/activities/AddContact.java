951d80e67c176f4eac029f070f546b0d08db1c18
ryotogashi <ryo.togashi.ca@gmail.com>
2019-05-13 22:45:05 -0700

    add Addcontact text and button.

--- android/Assignment3ContantsApp/app/src/main/java/com/ryotogashi/assignment3contantsapp/activities/AddContact.java
+++ android/Assignment3ContantsApp/app/src/main/java/com/ryotogashi/assignment3contantsapp/activities/AddContact.java
@@ -2,14 +2,36 @@ package com.ryotogashi.assignment3contantsapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ryotogashi.assignment3contantsapp.R;
 
 public class AddContact extends AppCompatActivity {
 
+    private TextView name;
+    private TextView phone;
+    public static final String TAG = AddContact.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        name = findViewById(R.id.nameTV);
+        phone = findViewById(R.id.phoneTV);
+    }
+
+    public void submit_data(View view) {
+
+        String nameStr = name.getText().toString();
+        String phoneStr = phone.getText().toString();
+
+        Log.d(TAG, "name: " + nameStr);
+        Log.d(TAG, "phone: " + phoneStr);
+
+
+
     }
 }
